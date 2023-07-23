e006f07fca2d22c309f5c06ffe64dfc3176cbccb
Rhythm Bhiwani <rhythm.devopspro@gmail.com>
2023-07-24 00:08:11 +0530

    Added env

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github Actions Tutorial - By DevOps Pro</p>
+        <p>
+          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
+          DevOps Pro
+        </p>
         <a
           className="App-link"
           href="https://youtube.com/@DevOpsPro"

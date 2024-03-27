eebf427e92b44b767afa1187ff4d471bb7623634
ganesh <ganesh@gmail.com>
2024-03-27 13:55:02 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
-          DevOps Pro
+          DevOps_Pro Ganesh sanap !!
         </p>
         <a
           className="App-link"

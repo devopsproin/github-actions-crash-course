1b6e7790043380f761e7377a06501e6cb0e6a6bd
ganesh <ganesh@gmail.com>
2024-03-27 11:28:15 +0530

    added youtube link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://youtube.com/@DevOpsPro"
+          href="https://www.youtube.com/@ganeshsanap4828/"
           target="_blank"
           rel="noopener noreferrer"
         >
